92526330bdcb177d2dbf9c57e468f8b1a4b04eb5
annegerben <annegerben@users.noreply.github.com>
2017-08-18 12:20:00 +0200

    Create uninstallsccm.cmd

---
+++ uninstallsccm.cmd
@@ -0,0 +1,33 @@
+@echo off
+:: +:::-----------------------------------------------------------------+
+::  Copyright (C) 2017 Anne Gerben van Assen
+:: +--------------+-----------------------------------------------------+
+::  File:          uninstall.cmd                        
+:: +--------------+-----------------------------------------------------+
+::  Purpose :      remove sccm client and open log
+:: +--------------+-----------------------------------------------------+
+::  Author:        Anne Gerben van Assen                                        
+:: +--------------+-----------------------------------------------------+
+::  Date:          17-08-2017
+:: +--------------+-----------------------------------------------------+
+::  Version:       1.0.0                                               
+:: +--------------+-----------------------------------------------------+
+::  File History:                                                      
+:: +-------+----------+-------------------------------------------------+ 
+::   Name    Date       Change                                      
+::  +-------+----------+------------------------------------------------+ 
+::   AG      17-08-2017 initial version
+::  +-------+----------+------------------------------------------------+
+:: +--------------------------------------------------------------------+
+title uninstall SCCM client
+cls
+pushd "%~dp0"
+echo rename logfile
+rename %systemdrive%\Windows\CCMsetup\logs\ccmsetup.log %systemdrive%\Windows\CCMsetup\logs\beforeuninstall_ccmsetup.log
+echo start uninstall
+cmd /c powershell.exe -Command "Start-Process cmd '/k cd /d %cd% && %systemdrive%\Windows\ccmsetup\ccmsetup.exe /uninstall && exit' -Verb RunAs"
+echo open logfile
+cd .. 
+trace32.exe %systemdrive%\Windows\CCMsetup\logs\ccmsetup.log
+pause 
+exit 
