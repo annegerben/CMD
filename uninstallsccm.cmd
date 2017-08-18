107ccd1023925e38ed845ca1b43a1efc9516367d
annegerben <annegerben@users.noreply.github.com>
2017-08-18 14:55:59 +0200

    fixed some bugs

--- uninstallsccm.cmd
+++ uninstallsccm.cmd
@@ -17,17 +17,17 @@
 ::   Name    Date       Change                                      
 ::  +-------+----------+------------------------------------------------+ 
 ::   AG      17-08-2017 initial version
+::   AG      19-08-2017 fixed some bugs
 ::  +-------+----------+------------------------------------------------+
 :: +--------------------------------------------------------------------+
 title uninstall SCCM client
 cls
 pushd "%~dp0"
-echo rename logfile
-rename %systemdrive%\Windows\CCMsetup\logs\ccmsetup.log %systemdrive%\Windows\CCMsetup\logs\beforeuninstall_ccmsetup.log
-echo start uninstall
-cmd /c powershell.exe -Command "Start-Process cmd '/k cd /d %cd% && %systemdrive%\Windows\ccmsetup\ccmsetup.exe /uninstall && exit' -Verb RunAs"
+echo rename logfile and start uninstall
+powershell.exe -Command "Start-Process cmd '/c rename %systemdrive%\Windows\CCMsetup\logs\ccmsetup.log beforeuninstall_ccmsetup.log && cd /d %cd% && %systemdrive%\Windows\ccmsetup\ccmsetup.exe /uninstall && exit' -Verb RunAs"
 echo open logfile
 cd .. 
-trace32.exe %systemdrive%\Windows\CCMsetup\logs\ccmsetup.log
+xcopy %CD:~0,3%\trace32.exe C:\dxc\ /y
+C:\dxc\trace32.exe %systemdrive%\Windows\CCMsetup\logs\ccmsetup.log
 pause 
-exit 
+exit
